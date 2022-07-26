497471971a29c72551a8758c869a9b82b9f31108
nurullah <nurullah7733@gmail.com>
2022-07-26 20:54:49 +0600

    separetFrontend

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ app.use('/api/v1', router);
 // ******************************************* Managing Front End Routing
 
 app.use(express.static('client/build'));
-app.get('*', function (req, res) {
+app.get('/', function (req, res) {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 });
 
